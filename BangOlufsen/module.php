7072de7709fee3585f8c42e4eb3f37d7b97aa410
Herwin Jan Steehouwer <herwin@steehouwer.nu>
2019-07-20 21:57:34 +0200

    3

--- BangOlufsen/module.php
+++ BangOlufsen/module.php
@@ -201,7 +201,7 @@ class BangOlufsenDevice extends BangOlufsenDeviceBase
         if ($type=="POST") curl_setopt($ch, CURLOPT_POST, true);
         if ($type=="PUT") { 
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-            curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data);
+            curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
         }
         $file_content = curl_exec($ch);
         curl_close($ch);
@@ -224,7 +224,7 @@ class BangOlufsenDevice extends BangOlufsenDeviceBase
                 $JsonString = json_encode($JSON);
                 $this->SendDataToParent($JsonString);
             }
-            if ($Date[0]==200)
+            if ($Data[0]==200)
             {
                 $this->__setNewValue("BOStatus",3);
 
