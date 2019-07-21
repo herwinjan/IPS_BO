3d2b087849b5b5203e079ffb0909d2b5146d6f63
Herwin Jan Steehouwer <herwin@steehouwer.nu>
2019-07-21 11:56:40 +0200

    devide classes added getVolume added getActiveSources

--- BangOlufsen/module.php
+++ BangOlufsen/module.php
@@ -1,32 +1,14 @@
 <?php
 
-/** 
- * @addtogroup BangOlufsen
- * @{
- *
- * @package       BangOlufsen
- * @file          module.php
- * @author        Herwin Jan Steehouwer <herwin@steehouwer.nu>
- * @copyright     2019 Herwin Jan Steehouwer
- * @license       https://creativecommons.org/licenses/by-nc-sa/4.0/ CC BY-NC-SA 4.0
- * @version       2.0
- *
- */
-
-class BangOlufsenDeviceBase extends IPSModule
-{
-
-}
+require_once(__DIR__ . "/../libs/beoclass.php");
 
 /**
- * NoTrigger Klasse für die die Überwachung einer Variable auf fehlende Änderung/Aktualisierung.
- * Erweitert BangOlufsenDevice.
  *
- * @package       BangOlufsen
+ * @package       BangOlufsen BoePlay devices
  * @author        Herwin Jan Steehouwer <herwin@steehouwer.nu>
  * @copyright     2019 Herwin Jan Steehouwer
  * @license       https://creativecommons.org/licenses/by-nc-sa/4.0/ CC BY-NC-SA 4.0
- * @version       2.0
+ * @version       0.1
  * @example <b>None</b>
  *
  * @property int $State Letzer Zustand
@@ -34,40 +16,6 @@ class BangOlufsenDeviceBase extends IPSModule
  */
 class BangOlufsenDevice extends BangOlufsenDeviceBase
 {
-    
-        /**
-     * Wert einer Eigenschaft aus den InstanceBuffer lesen.
-     *
-     * @access public
-     * @param string $name Propertyname
-     * @return mixed Value of Name
-     */
-    public function __get($name)
-    {
-        return unserialize($this->GetBuffer($name));
-    }
-    /**
-     * Wert einer Eigenschaft in den InstanceBuffer schreiben.
-     *
-     * @access public
-     * @param string $name Propertyname
-     * @param mixed Value of Name
-     */
-    public function __set($name, $value)
-    {
-        $this->SetBuffer($name, serialize($value));
-    }
-
-
-    public function __construct($InstanceID)
-    {
-        //Never delete this line!
-        parent::__construct($InstanceID);
-        
-        //You can add custom code below.
-        
-        
-    }
 
     public function Create()
     {
@@ -126,6 +74,8 @@ class BangOlufsenDevice extends BangOlufsenDeviceBase
         $this->VolumeMin=0;
         $this->VolumeMax=100;
         $this->Type=0;
+        $this->jid="";
+        $this->BeoName="";
         
         
         if ((float) IPS_GetKernelVersion() < 4.2) {
@@ -142,9 +92,9 @@ class BangOlufsenDevice extends BangOlufsenDeviceBase
        // $this->RegisterMessage($data['ConnectionID'], IM_DISCONNECT);
        // $this->RegisterMessage($data['ConnectionID'], IM_CHANGESTATUS);
 
+       $this->getActiveSources();
+       $this->getVolume();
        
-       parent::ApplyChanges();
-        
 
     }
 
@@ -192,28 +142,7 @@ class BangOlufsenDevice extends BangOlufsenDeviceBase
 
     }
 
-    public function __sendCommand($url, $data,$type="POST")
-    {
-        $ch = curl_init();
-        $timeout = 5;
-        curl_setopt($ch, CURLOPT_URL, "http://".$this->ReadPropertyString('IP').":".$this->ReadPropertyInteger('Port')."/". $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json'
-        ));
-        if ($type=="POST") curl_setopt($ch, CURLOPT_POST, true);
-        if ($type=="PUT") { 
-
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-            curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
-        }
-        $file_content = curl_exec($ch);
-        $this->SendDebug(__FUNCTION__,"CURL ".$file_content,0);
-        curl_close($ch);
-    }
-
+    
     public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
     {
         IPS_LogMessage("BO RECV", $Message);
@@ -328,6 +257,7 @@ class BangOlufsenDevice extends BangOlufsenDeviceBase
                             $this->__setNewValue("BOSong","");
                             $this->__setNewValue("BOArtist","");
                             $this->__setNewValue("BOLoc","");
+                            $this->__setNewValue("BOSource","Shutdown");
                             $this->__setStatus("stop");
                         }
                         break;
@@ -353,12 +283,7 @@ class BangOlufsenDevice extends BangOlufsenDeviceBase
                         $level=$command["notification"]["data"]["speaker"]["level"];
                         $min=$command["notification"]["data"]["speaker"]["range"]["minimum"];
                         $max=$command["notification"]["data"]["speaker"]["range"]["maximum"];
-                        $this->VolumeMin=$min;
-                        $this->VolumeMax=$max;
-                                               
-                        $new=round(($level - $min) * 100) / ($max - $min);
-                                                 
-                        $this->__setNewValue("BOVolume",$new);
+                        $this->setVolume($level,$min,$max);
                         break;
 
                 }
@@ -385,118 +310,5 @@ class BangOlufsenDevice extends BangOlufsenDeviceBase
         $this->__setNewValue("BOStatus",$st);
     }
 
-    private function __setNewValue($name, $value)
-    {
-        $sid = @IPS_GetObjectIDByIdent($name, $this->InstanceID);
-       // $this->SendDebug(__FUNCTION__, "SV: ".$name." -> ".$sid." -> ".$value,0);
-        if ($sid) SetValue($sid, $value);
-    }
-    
-    private function __CreateCategory($Name, $Ident = '', $ParentID = 0)
-    {
-        $RootCategoryID = $this->InstanceID;
-        IPS_LogMessage("Visonic DEBUG", "CreateCategory: ( $Name, $Ident, $ParentID ) \n");
-        if ('' != $Ident) {
-            $CatID = @IPS_GetObjectIDByIdent($Ident, $ParentID);
-            if (false !== $CatID) {
-                $Obj = IPS_GetObject($CatID);
-                if (0 == $Obj['ObjectType']) { // is category?
-                    return $CatID;
-                }
-            }
-        }
-        $CatID = IPS_CreateCategory();
-        IPS_SetName($CatID, $Name);
-        IPS_SetIdent($CatID, $Ident);
-        if (0 == $ParentID) {
-            if (IPS_ObjectExists($RootCategoryID)) {
-                $ParentID = $RootCategoryID;
-            }
-        }
-        IPS_SetParent($CatID, $ParentID);
-        return $CatID;
-    }
-
-    private function __CreateVariable($Name, $Type, $Value, $Ident = '', $ParentID = 0)
-    {
-        IPS_LogMessage("Visonic DEBUG", "CreateVariable: ( $Name, $Type, $Value, $Ident, $ParentID ) \n");
-        if ('' != $Ident) {
-            $VarID = @IPS_GetObjectIDByIdent($Ident, $ParentID);
-            if (false !== $VarID) {
-                $this->__SetVariable($VarID, $Type, $Value);
-                return $VarID;
-            }
-        }
-        $VarID = @IPS_GetObjectIDByName($Name, $ParentID);
-        if (false !== $VarID) { // exists?
-            $Obj = IPS_GetObject($VarID);
-            if (2 == $Obj['ObjectType']) { // is variable?
-                $Var = IPS_GetVariable($VarID);
-                if ($Type == $Var['VariableValue']['ValueType']) {
-                    $this->__SetVariable($VarID, $Type, $Value);
-                    return $VarID;
-                }
-            }
-        }
-        $VarID = IPS_CreateVariable($Type);
-        IPS_SetParent($VarID, $ParentID);
-        IPS_SetName($VarID, $Name);
-        if ('' != $Ident) {
-            IPS_SetIdent($VarID, $Ident);
-        }
-        $this->__SetVariable($VarID, $Type, $Value);
-        return $VarID;
-    }
-
-    private function __SetVariable($VarID, $Type, $Value)
-    {
-        switch ($Type) {
-            case 0: // boolean
-                SetValueBoolean($VarID, $Value);
-                break;
-            case 1: // integer
-                SetValueInteger($VarID, $Value);
-                break;
-            case 2: // float
-                SetValueFloat($VarID, $Value);
-                break;
-            case 3: // string
-                SetValueString($VarID, $Value);
-                break;
-        }
-    }
-    protected function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize) {
-        
-        if(!IPS_VariableProfileExists($Name)) {
-            IPS_CreateVariableProfile($Name, 1);
-        } else {
-            $profile = IPS_GetVariableProfile($Name);
-            if($profile['ProfileType'] != 1)
-            throw new Exception("Variable profile type does not match for profile ".$Name);
-        }
-        
-        IPS_SetVariableProfileIcon($Name, $Icon);
-        IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
-        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
-        
-    }
-    protected function RegisterProfileIntegerEx($Name, $Icon, $Prefix, $Suffix, $Associations) {
-        if ( sizeof($Associations) === 0 ){
-            $MinValue = 0;
-            $MaxValue = 0;
-        } else {
-            $MinValue = $Associations[0][0];
-            $MaxValue = $Associations[sizeof($Associations)-1][0];
-        }
-        
-        $this->RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, 0);
-        
-        foreach($Associations as $Association) {
-            IPS_SetVariableProfileAssociation($Name, $Association[0], $Association[1], $Association[2], $Association[3]);
-        }
-        
-    }
-
    
 }
-/** @} */
---
+++ libs/beoclass.php
@@ -0,0 +1,234 @@
+<?php
+
+/** 
+ * @addtogroup BangOlufsen
+ * @{
+ *
+ * @package       BangOlufsen BeoPlay devices
+ * @file          module.php
+ * @author        Herwin Jan Steehouwer <herwin@steehouwer.nu>
+ * @copyright     2019 Herwin Jan Steehouwer
+ * @license       https://creativecommons.org/licenses/by-nc-sa/4.0/ CC BY-NC-SA 4.0
+ * @version       0.1
+ *
+ */
+
+class BangOlufsenDeviceBase extends IPSModule
+{
+
+    public function getActiveSources()
+    {
+        $body=$this->__sendCommand("BeoZone/Zone/ActiveSources/","","GET");
+        $js=explode("\n",$data->Buffer);
+        foreach ( $js as $j)
+        {
+            if ($j[0]=='{')
+            {
+                $command = json_decode(trim(utf8_decode($j)),TRUE);   
+                if (@$command["primaryExperience"]["source"]["friendlyName"])
+                    $this->__setNewValue("BOSource",$command["primaryExperience"]["source"]["friendlyName"]);
+                if (@$command["primaryExperience"]["source"]["product"]["jid"])
+                    $this->jid=$command["primaryExperience"]["source"]["product"]["jid"];
+                if (@$command["primaryExperience"]["source"]["product"]["friendlyName"])
+                    $this->jid=$command["primaryExperience"]["source"]["product"]["friendlyName"];
+                
+                
+            }
+        }        
+    }
+    public function getVolume()
+    {
+        $body=$this->__sendCommand("/BeoZone/Zone/Sound/Volume","","GET");
+        $js=explode("\n",$data->Buffer);
+        foreach ( $js as $j)
+        {
+            if ($j[0]=='{')
+            {
+                $command = json_decode(trim(utf8_decode($j)),TRUE);   
+                if (@$command["volume"]["speaker"]["range"]["maximum"])
+                    $max=$command["volume"]["speaker"]["range"]["maximum"];
+                if (@$command["volume"]["speaker"]["range"]["minimum"])
+                    $min=$command["volume"]["speaker"]["range"]["minimum"];
+                if (@$command["volume"]["speaker"]["level"])
+                    $level=$command["volume"]["speaker"]["level"];
+                $this->setVolume($level, $min, $max);                
+            }
+        }        
+    }
+    public function setVolume($level, $min, $max)
+    {
+        $this->VolumeMin=$min;
+        $this->VolumeMax=$max;
+                               
+        $new=round(($level - $min) * 100) / ($max - $min);
+                                 
+        $this->__setNewValue("BOVolume",$new);
+    }
+
+    
+
+        /**
+     * Wert einer Eigenschaft aus den InstanceBuffer lesen.
+     *
+     * @access public
+     * @param string $name Propertyname
+     * @return mixed Value of Name
+     */
+    public function __get($name)
+    {
+        return unserialize($this->GetBuffer($name));
+    }
+    /**
+     * Wert einer Eigenschaft in den InstanceBuffer schreiben.
+     *
+     * @access public
+     * @param string $name Propertyname
+     * @param mixed Value of Name
+     */
+    public function __set($name, $value)
+    {
+        $this->SetBuffer($name, serialize($value));
+    }
+
+    public function __sendCommand($url, $data,$type="POST")
+    {
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch, CURLOPT_URL, "http://".$this->ReadPropertyString('IP').":".$this->ReadPropertyInteger('Port')."/". $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json'
+        ));
+        if ($type=="POST") curl_setopt($ch, CURLOPT_POST, true);
+        if ($type=="PUT") { 
+
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+            curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+        }
+        if ($type=="GET")
+        {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        }
+        $file_content = curl_exec($ch);
+        $this->SendDebug(__FUNCTION__,"CURL ".$file_content,0);
+        curl_close($ch);
+        return $file_content;
+    }
+
+    private function __setNewValue($name, $value)
+    {
+        $sid = @IPS_GetObjectIDByIdent($name, $this->InstanceID);
+       // $this->SendDebug(__FUNCTION__, "SV: ".$name." -> ".$sid." -> ".$value,0);
+        if ($sid) SetValue($sid, $value);
+    }
+    
+    private function __CreateCategory($Name, $Ident = '', $ParentID = 0)
+    {
+        $RootCategoryID = $this->InstanceID;
+        IPS_LogMessage("Visonic DEBUG", "CreateCategory: ( $Name, $Ident, $ParentID ) \n");
+        if ('' != $Ident) {
+            $CatID = @IPS_GetObjectIDByIdent($Ident, $ParentID);
+            if (false !== $CatID) {
+                $Obj = IPS_GetObject($CatID);
+                if (0 == $Obj['ObjectType']) { // is category?
+                    return $CatID;
+                }
+            }
+        }
+        $CatID = IPS_CreateCategory();
+        IPS_SetName($CatID, $Name);
+        IPS_SetIdent($CatID, $Ident);
+        if (0 == $ParentID) {
+            if (IPS_ObjectExists($RootCategoryID)) {
+                $ParentID = $RootCategoryID;
+            }
+        }
+        IPS_SetParent($CatID, $ParentID);
+        return $CatID;
+    }
+
+    private function __CreateVariable($Name, $Type, $Value, $Ident = '', $ParentID = 0)
+    {
+        IPS_LogMessage("Visonic DEBUG", "CreateVariable: ( $Name, $Type, $Value, $Ident, $ParentID ) \n");
+        if ('' != $Ident) {
+            $VarID = @IPS_GetObjectIDByIdent($Ident, $ParentID);
+            if (false !== $VarID) {
+                $this->__SetVariable($VarID, $Type, $Value);
+                return $VarID;
+            }
+        }
+        $VarID = @IPS_GetObjectIDByName($Name, $ParentID);
+        if (false !== $VarID) { // exists?
+            $Obj = IPS_GetObject($VarID);
+            if (2 == $Obj['ObjectType']) { // is variable?
+                $Var = IPS_GetVariable($VarID);
+                if ($Type == $Var['VariableValue']['ValueType']) {
+                    $this->__SetVariable($VarID, $Type, $Value);
+                    return $VarID;
+                }
+            }
+        }
+        $VarID = IPS_CreateVariable($Type);
+        IPS_SetParent($VarID, $ParentID);
+        IPS_SetName($VarID, $Name);
+        if ('' != $Ident) {
+            IPS_SetIdent($VarID, $Ident);
+        }
+        $this->__SetVariable($VarID, $Type, $Value);
+        return $VarID;
+    }
+
+    private function __SetVariable($VarID, $Type, $Value)
+    {
+        switch ($Type) {
+            case 0: // boolean
+                SetValueBoolean($VarID, $Value);
+                break;
+            case 1: // integer
+                SetValueInteger($VarID, $Value);
+                break;
+            case 2: // float
+                SetValueFloat($VarID, $Value);
+                break;
+            case 3: // string
+                SetValueString($VarID, $Value);
+                break;
+        }
+    }
+    protected function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize) {
+        
+        if(!IPS_VariableProfileExists($Name)) {
+            IPS_CreateVariableProfile($Name, 1);
+        } else {
+            $profile = IPS_GetVariableProfile($Name);
+            if($profile['ProfileType'] != 1)
+            throw new Exception("Variable profile type does not match for profile ".$Name);
+        }
+        
+        IPS_SetVariableProfileIcon($Name, $Icon);
+        IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
+        
+    }
+    protected function RegisterProfileIntegerEx($Name, $Icon, $Prefix, $Suffix, $Associations) {
+        if ( sizeof($Associations) === 0 ){
+            $MinValue = 0;
+            $MaxValue = 0;
+        } else {
+            $MinValue = $Associations[0][0];
+            $MaxValue = $Associations[sizeof($Associations)-1][0];
+        }
+        
+        $this->RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, 0);
+        
+        foreach($Associations as $Association) {
+            IPS_SetVariableProfileAssociation($Name, $Association[0], $Association[1], $Association[2], $Association[3]);
+        }
+        
+    }
+
+
+}
+/** @} */
